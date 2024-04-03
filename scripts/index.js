38e94c57c1c78c0cdbcb7764f13cbabc716a6cca
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-04-03 10:52:35 +0200

    Исправлениебага отображения карточек

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,18 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 const content = document.querySelector(".content");
-const sectionPlaces = content.querySelector(".places");
+const sectionPlaces = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function creatingCard(name, link) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__title").textContent = name;
+function creatingCard(link, name) {
+  const templateElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  templateElement.querySelector(".card__image").src = link;
+  templateElement.querySelector(".card__image").alt = name;
+  templateElement.querySelector(".card__title").textContent = name;
 
-  return cardElement;
+  return templateElement;
 }
 
 // @todo: Функция удаления карточки
@@ -22,12 +25,11 @@ function deletingCard(event) {
 // @todo: Вывести карточки на страницу
 function showCard() {
   initialCards.forEach((element) => {
-    const el = creatingCard(element.name, element.link);
-    el.querySelector(".card__delete-button").addEventListener(
-      "click",
-      deletingCard
-    );
-    sectionPlaces.append(el);
+    const cardItem = creatingCard(element.link, element.name);
+    cardItem
+      .querySelector(".card__delete-button")
+      .addEventListener("click", deletingCard);
+    sectionPlaces.append(cardItem);
   });
 }
 
